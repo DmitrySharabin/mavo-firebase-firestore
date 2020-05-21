457c21de300b590324408b2419da18048e3c46d5
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-05-21 17:23:34 +0300

    Update plugin for mavo.io

--- mavo-firebase-firestore.js
+++ mavo-firebase-firestore.js
@@ -1,1 +1,322 @@
-"use strict";function _slicedToArray(a,b){return _arrayWithHoles(a)||_iterableToArrayLimit(a,b)||_nonIterableRest()}function _nonIterableRest(){throw new TypeError("Invalid attempt to destructure non-iterable instance")}function _iterableToArrayLimit(a,b){if(Symbol.iterator in Object(a)||"[object Arguments]"===Object.prototype.toString.call(a)){var c=[],d=!0,e=!1,f=void 0;try{for(var g,h=a[Symbol.iterator]();!(d=(g=h.next()).done)&&(c.push(g.value),!(b&&c.length===b));d=!0);}catch(a){e=!0,f=a}finally{try{d||null==h["return"]||h["return"]()}finally{if(e)throw f}}return c}}function _arrayWithHoles(a){if(Array.isArray(a))return a}function asyncGeneratorStep(a,b,c,d,e,f,g){try{var h=a[f](g),i=h.value}catch(a){return void c(a)}h.done?b(i):Promise.resolve(i).then(d,e)}function _asyncToGenerator(a){return function(){var b=this,c=arguments;return new Promise(function(d,e){function f(a){asyncGeneratorStep(h,d,e,f,g,"next",a)}function g(a){asyncGeneratorStep(h,d,e,f,g,"throw",a)}var h=a.apply(b,c);f(void 0)})}}(function(a){var b=Mavo.Backend.register(a.Class({extends:Mavo.Backend,id:"Firebase",constructor:function(c,d){var e=this,f=d.mavo,g=d.format;this.permissions.on("read"),this.defaults={collectionName:"mavo-apps",filename:f.id,storageName:f.element.getAttribute("mv-firebase-storage")||f.id,features:{auth:!0,storage:!0}},a.extend(this,this.defaults),template=f.element.getAttribute("mv-firebase")||"",this.features=b.getFeatures(template,this.features),this.features.auth?this.permissions.on("login"):this.permissions.on(["edit","save"]),this.ready=a.load("https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js").then(_asyncToGenerator(regeneratorRuntime.mark(function c(){var d;return regeneratorRuntime.wrap(function(c){for(;;)switch(c.prev=c.next){case 0:return c.next=2,Promise.all([a.load("https://www.gstatic.com/firebasejs/7.8.2/firebase-firestore.js"),a.include(!e.features.storage,"https://www.gstatic.com/firebasejs/7.8.2/firebase-storage.js"),a.include(!e.features.auth,"https://www.gstatic.com/firebasejs/7.8.2/firebase-auth.js")]);case 2:return a.extend(e,b.parseURL(e.source,e.defaults)),d={apiKey:f.element.getAttribute("mv-firebase-key"),databaseURL:e.databaseURL,projectId:e.projectId,authDomain:"".concat(e.projectId,".firebaseapp.com"),storageBucket:"".concat(e.projectId,".appspot.com")},e.app=firebase.apps.length?firebase.initializeApp(d,f.id):firebase.initializeApp(d),e.db=e.app.firestore().collection(e.collectionName),f.element.hasAttribute("mv-firebase-realtime")?e.unsubscribe=e.db.doc(e.filename).onSnapshot(function(a){a.metadata.hasPendingWrites?"Local":"Server";f.render(a.data())},function(a){return f.error("Firebase: ".concat(a.message))}):e.unsubscribe&&e.unsubscribe(),e.features.storage&&(e.storageBucketRef=e.app.storage().ref()),e.features.auth&&e.app.auth().onAuthStateChanged(function(b){b?(e.user={username:b.email,name:b.displayName,avatar:b.photoURL,user:b},a.fire(f.element,"mv-login",{backend:e}),e.permissions.off("login").on(["edit","save","logout"])):(e.user=null,a.fire(f.element,"mv-logout",{backend:e}),e.permissions.off(["edit","add","delete","save","logout"]).on("login"))}),c.abrupt("return",Promise.resolve());case 10:case"end":return c.stop();}},c)}))),this.features.auth&&this.login(!0)},update:function(c,d){this["super"].update.call(this,c,d),a.extend(this,b.parseURL(this.source,this.defaults))},get:function(a){return this.db.doc(a).get()},load:function(){var a=this;return this.ready.then(function(){return a.get(a.filename).then(function(a){return Promise.resolve(a.data()||{})})["catch"](function(b){return a.mavo.error("Firebase: ".concat(b.message))})})},put:function(a){var b=1<arguments.length&&arguments[1]!==void 0?arguments[1]:this.path,c=2<arguments.length&&arguments[2]!==void 0?arguments[2]:{};return this.storageBucketRef.child(b).put(a).then(function(a){return a.ref.getDownloadURL()})},upload:function(a,b){return b="".concat(this.storageName,"/").concat(b),this.put(a,b).then(function(a){return a})},store:function(a){var b=this,c=1<arguments.length&&arguments[1]!==void 0?arguments[1]:{},d=c.path,e=c.format,f=void 0===e?this.format:e;return this.db.doc(this.filename).set(a).then(function(){return Promise.resolve()})["catch"](function(a){return b.mavo.error("Firebase: ".concat(a.message))})},login:function(a){var b=this;return this.ready.then(function(){return b.user?Promise.resolve():new Promise(function(c,d){if(a)c(b.user);else{var e=new firebase.auth.GoogleAuthProvider;e.addScope("https://www.googleapis.com/auth/cloud-platform"),firebase.auth().useDeviceLanguage(),b.app.auth().signInWithPopup(e)["catch"](function(a){b.mavo.error("Firebase: ".concat(a.message)),d(a)})}})})},logout:function(){var a=this;return this.app.auth().signOut()["catch"](function(b){a.mavo.error("Firebase: ".concat(b.message))})},static:{test:function(a){return /^https:\/\/.*\.firebaseio\.com(\/)?/.test(a.trim())},parseURL:function(a){var b=1<arguments.length&&void 0!==arguments[1]?arguments[1]:{},c={},d=new URL(a);c.databaseURL=d.hostname,c.projectId=d.hostname.split(".").shift();var e=d.pathname.slice(1);if(-1<e.indexOf("/")){var f=e.split("/"),g=_slicedToArray(f,2);c.collectionName=g[0],c.filename=g[1]}else c.collectionName=b.collectionName,c.filename=e||b.filename;return c},getFeatures:function(a,b){var c=b,d=Object.keys(b);if(a&&(a=a.trim())){var e=/^with\s|\bno-\w+\b/.test(a),f=a.split(/\s+/);return f=Mavo.Functions.unique(f.reverse()).reverse(),e&&(f=d.filter(function(a){var b=Math.max,c=f.lastIndexOf(a),d=f.lastIndexOf("no-"+a),e=c>b(-1,d),g=d>b(-1,c);return e||!g})),d.forEach(function(a){return f.includes(a)?c[a]=!0:c[a]=!1}),c}return c}}}))})(Bliss);
+// @ts-check
+
+/**
+ * Firebase backend plugin for Mavo
+ * @author Dmitry Sharabin and contributors
+ * @version %%VERSION%%
+ */
+(function($) {
+	"use strict";
+
+	const _ = Mavo.Backend.register(
+		$.Class({
+			extends: Mavo.Backend,
+
+			id: "Firebase",
+
+			constructor: function(url, { mavo, format }) {
+				// Initialization code
+				this.permissions.on("read");
+
+				this.defaults = {
+					collectionName: "mavo-apps",
+					filename: mavo.id,
+					storageName:
+						mavo.element.getAttribute("mv-firebase-storage") || mavo.id,
+					features: {
+						auth: true,
+						storage: true
+					}
+				};
+
+				$.extend(this, this.defaults);
+
+				// Which backend features should we support?
+				const template = mavo.element.getAttribute("mv-firebase") || "";
+
+				this.features = _.getFeatures(template, this.features);
+
+				if (this.features.auth) {
+					this.permissions.on("login");
+				}
+				else {
+					this.permissions.on(["edit", "save"]);
+				}
+
+				this.ready =
+					// First of all, we need to download the Firebase core file
+					$.load(
+						"https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js"
+					).then(async () => {
+						// Then download the other parts if needed
+						await Promise.all([
+							// Cloud Firestore
+							$.load(
+								"https://www.gstatic.com/firebasejs/7.8.2/firebase-firestore.js"
+							),
+
+							// Cloud Storage
+							$.include(
+								!this.features.storage,
+								"https://www.gstatic.com/firebasejs/7.8.2/firebase-storage.js"
+							),
+
+							// Authentication
+							$.include(
+								!this.features.auth,
+								"https://www.gstatic.com/firebasejs/7.8.2/firebase-auth.js"
+							)
+						]);
+
+						// Get all the config info from the raw URL (attribute value) and the corresponding attribute
+						$.extend(this, _.parseURL(this.source, this.defaults));
+
+						// The app's Firebase configuration
+						const config = {
+							apiKey: mavo.element.getAttribute("mv-firebase-key"),
+							databaseURL: this.databaseURL,
+							projectId: this.projectId,
+							authDomain: `${this.projectId}.firebaseapp.com`,
+							storageBucket: `${this.projectId}.appspot.com`
+						};
+
+						// Initialize Cloud Firestore through Firebase
+						// Support using multiple apps on the same page
+						if (!firebase.apps.length) {
+							this.app = firebase.initializeApp(config);
+						}
+						else {
+							this.app = firebase.initializeApp(config, mavo.id);
+						}
+
+						this.db = this.app.firestore().collection(this.collectionName);
+
+						if (mavo.element.hasAttribute("mv-firebase-realtime")) {
+							// Get realtime updates
+							this.unsubscribe = this.db.doc(this.filename).onSnapshot(
+								doc => {
+									const source = doc.metadata.hasPendingWrites
+										? "Local"
+										: "Server";
+									// TODO: There's the problem of what to do when local edits conflict with pulled data
+									// if (source === "Server" && ...) {...}
+									mavo.render(doc.data());
+								},
+								error => mavo.error(`Firebase: ${error.message}`)
+							);
+						}
+						else if (this.unsubscribe) {
+							// Stop listening to changes
+							this.unsubscribe();
+						}
+
+						if (this.features.storage) {
+							// Get a reference to the storage service, which is used to create references in the storage bucket,
+							// and create a storage reference from the storage service
+							this.storageBucketRef = this.app.storage().ref();
+						}
+
+						if (this.features.auth) {
+							// Set an authentication state observer and get user data
+							this.app.auth().onAuthStateChanged(user => {
+								if (user) {
+									// User is signed in
+									this.user = {
+										username: user.email,
+										name: user.displayName,
+										avatar: user.photoURL,
+										user // raw user object
+									};
+
+									$.fire(mavo.element, "mv-login", { backend: this });
+
+									this.permissions.off("login").on(["edit", "save", "logout"]);
+								}
+								else {
+									// User is signed out
+									this.user = null;
+
+									$.fire(mavo.element, "mv-logout", { backend: this });
+
+									this.permissions
+										.off(["edit", "add", "delete", "save", "logout"])
+										.on("login");
+								}
+							});
+						}
+
+						return Promise.resolve();
+					});
+
+				if (this.features.auth) {
+					this.login(true);
+				}
+			},
+
+			update: function(url, o) {
+				this.super.update.call(this, url, o);
+
+				$.extend(this, _.parseURL(this.source, this.defaults));
+			},
+
+			get: function(url) {
+				return this.db.doc(url).get();
+			},
+
+			load: function() {
+				return this.ready.then(() =>
+					this.get(this.filename)
+						.then(doc => Promise.resolve(doc.data() || {}))
+						.catch(error => this.mavo.error(`Firebase: ${error.message}`))
+				);
+			},
+
+			/**
+			 * Low-level saving code
+			 * @param {*} serialized Data serialized according to this.format
+			 * @param {*} path Path to store data
+			 * @param {*} o Arbitrary options
+			 */
+			put: function(serialized, path = this.path, o = {}) {
+				return this.storageBucketRef
+					.child(path)
+					.put(serialized)
+					.then(snapshot => snapshot.ref.getDownloadURL());
+			},
+
+			/**
+			 * Upload code
+			 * @param {*} file File object to be uploaded
+			 * @param {*} path Relative path to store uploads (e.g. "images")
+			 */
+			upload: function(file, path) {
+				path = `${this.storageName}/${path}`;
+
+				return this.put(file, path).then(downloadURL => downloadURL);
+			},
+
+			store: function(data, { path, format = this.format } = {}) {
+				return this.db
+					.doc(this.filename)
+					.set(data)
+					.then(() => Promise.resolve())
+					.catch(error => this.mavo.error(`Firebase: ${error.message}`));
+			},
+
+			// Takes care of authentication. If passive is true, only checks if
+			// the user is already logged in, but does not present any login UI
+			login: function(passive) {
+				return this.ready.then(() => {
+					if (this.user) {
+						return Promise.resolve();
+					}
+
+					return new Promise((resolve, reject) => {
+						if (passive) {
+							resolve(this.user);
+						}
+						else {
+							const provider = new firebase.auth.GoogleAuthProvider();
+
+							// Specify additional OAuth 2.0 scopes that we need to request to access Google APIs
+							provider.addScope(
+								"https://www.googleapis.com/auth/cloud-platform"
+							);
+
+							// Apply the default browser preference
+							firebase.auth().useDeviceLanguage();
+
+							this.app
+								.auth()
+								.signInWithPopup(provider)
+								.catch(error => {
+									this.mavo.error(`Firebase: ${error.message}`);
+									reject(error);
+								});
+						}
+					});
+				});
+			},
+
+			// Log current user out
+			logout: function() {
+				return this.app
+					.auth()
+					.signOut()
+					.catch(error => {
+						this.mavo.error(`Firebase: ${error.message}`);
+					});
+			},
+
+			static: {
+				// Mandatory and very important! This determines when the backend is used
+				// value: The mv-storage/mv-source/mv-init value
+				test: function(value) {
+					// Returns true if this value applies to this backend
+					return /^https:\/\/.*\.firebaseio\.com(\/)?/.test(value.trim());
+				},
+
+				// Parse the mv-storage/mv-source/mv-init value, return Firebase database URL, project id, collection name, filename
+				parseURL: function(source, defaults = {}) {
+					const ret = {};
+
+					const url = new URL(source);
+
+					ret.databaseURL = url.hostname;
+					ret.projectId = url.hostname.split(".").shift();
+
+					const path = url.pathname.slice(1);
+					if (path.indexOf("/") > -1) {
+						const parts = path.split("/");
+
+						[ret.collectionName, ret.filename] = parts;
+					}
+					else {
+						ret.collectionName = defaults.collectionName;
+						ret.filename = path || defaults.filename;
+					}
+
+					return ret;
+				},
+
+				/**
+				 * Parse the list of features the Firebase backend should support
+				 * @param {*} template The value of the mv-firebase attribute or an empty string
+				 */
+				getFeatures: function(template, defaults) {
+					const ret = defaults;
+
+					const all = Object.keys(defaults);
+
+					if (template && (template = template.trim())) {
+						const relative = /^with\s|\bno-\w+\b/.test(template);
+						let ids = template.split(/\s+/);
+
+						// Drop duplicates (last one wins)
+						ids = Mavo.Functions.unique(ids.reverse()).reverse();
+
+						if (relative) {
+							ids = all.filter(id => {
+								const positive = ids.lastIndexOf(id);
+								const negative = ids.lastIndexOf("no-" + id);
+								const keep = positive > Math.max(-1, negative);
+								const drop = negative > Math.max(-1, positive);
+
+								return keep || !drop;
+							});
+						}
+
+						all.forEach(id =>
+							ids.includes(id) ? (ret[id] = true) : (ret[id] = false)
+						);
+
+						return ret;
+					}
+
+					// No template, return default set
+					return ret;
+				}
+			}
+		})
+	);
+})(Bliss);
